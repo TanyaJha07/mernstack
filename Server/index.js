31c51ade75d6c9e58c554202e284768ce7ea644e
Tanya Jha <tanyajhaanni@gmail.com.com>
2024-08-21 08:19:49 +0530

    comments

--- Server/index.js
+++ Server/index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const TodoModel = require('./Models/Todos')
+const express = require('express'); //express = server
+const mongoose = require('mongoose'); //mongoose = mongodb
+const cors = require('cors'); //cors = security cross origin resource sharing
+const TodoModel = require('./Models/Todos') //require = import
 
 const app = express();
 app.use(cors())
-app.use(express.json());
-require('dotenv').config()
+app.use(express.json()); //express.json() = middleware for parsing JSON request bodies to object 
+require('dotenv').config() //dotenv = environment variable 
 db_url= process.env.db_url
 console.log(db_url)
 
 mongoose.connect(db_url)
 
-app.get('/get', (req,res) =>{
+app.get('/get', (req,res) =>{ //req=request, res = response
     TodoModel.find()
     .then(result=>res.json(result))
     .catch(err=>res.json(err))
 })
 
-app.delete('/delete/:id',(req,res)=>{
-    const {id}=req.params;
+app.delete('/delete/:id',(req,res)=>{ //req=postman frontend
+    const {id}=req.params; //params = parameter,url
     TodoModel.findByIdAndDelete({_id:id})
-    .then(result=>res.json(result))
+    .then(result=>res.json(result)) //deleted data from mongodb to frontend
     .catch(err=>res.json(err))
 }
 ) 
--- todo/learn.md
+++ todo/learn.md
@@ -5,3 +5,20 @@ windows+left arrow for dual window
 modularity - 1 file 1 function(multiple thing diff)
 rafce - react arrow function arrow component
 alt z = line break wrap text
+
+
+INITIAL SETUP FRONTEND
+1st step-
+## npm create vite@latest
+## project name
+## select react and js
+
+2nd STEP - frontend
+## cd project name
+## npm i
+## npm run dev
+
+START - 
+## INDEX.HTML IS CREATED(ALREADY)
+
+in react if u r using library store it in a variable const
